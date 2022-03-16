bf595dc6151bc04f4217665eaf2abdfe4917093d
Tosam01 <tosam0001@gmail.com>
2022-03-16 22:51:05 +0000

    task 2&3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * check lower case
+ * check for lowercase character 
  *
  */
 int _islower(int c)
 {
-	if ((c >= 97) && (c <=122))
+	if ((c >= 97) && (c <= 122))
 	{
 		return (1);
 	}

a3c523c9d62b2c494c280c24344b1dfd5344bb3e
Tosam01 <tosam0001@gmail.com>
2022-04-12 14:24:44 +0000

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints the phrase "Programming is like building a multiligual puzzle
- * Return: 0 if existed properly, non-zero otherwise
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

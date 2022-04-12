66bfc08299f3302c70728783eb80544aba587865
Tosam01 <tosam0001@gmail.com>
2022-04-12 00:00:42 +0000

    tk0-5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,6 +24,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	for (lname = 0; name[lname]; lname++)
 		;
+	
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
+
 	p_dog->name = malloc(lname + 1);
 	p_dog->owner = malloc(lowner + 1);
 
@@ -34,6 +38,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p_dog);
 		return (NULL);
 	}
+
 	for (i = 0; i < lname; i++)
 		p_dog->name[i] = name [i];
 	p_dog->name[i] = '\0';

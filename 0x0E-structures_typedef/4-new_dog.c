ac3f53972a5e3d5b72be1aba237be7d68d9a85be
Tosam01 <tosam0001@gmail.com>
2022-04-12 00:04:56 +0000

    tk0-5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,11 +3,6 @@
 
 /**
  * new_dog - creates a new dog.
-#include "dog.h"
-#include <stdlib.h>
-
-/**
- * new_dog - creates a new dog.
  * @name: name of the dog.
  * @age: age of the dog.
  * @owner: owner of the dog.

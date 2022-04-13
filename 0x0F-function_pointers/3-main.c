d0b426fdad080a46721aa05c3baeb7964e258102
Tosam01 <tosam0001@gmail.com>
2022-04-13 22:59:35 +0000

    correction

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,11 +1,11 @@
 #include "3-calc.h"
 
 /**
- * main - check the code for Holberton School students
- * @argc: argument count
- * @argv: argument vector
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
@@ -23,6 +23,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+
 	operation = get_op_func(argv[2]);
 
 	if (operation == NULL)

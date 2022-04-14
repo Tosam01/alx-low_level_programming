801f6214542c0267779ef5d93c2f9a8be25aaa00
Tosam01 <tosam0001@gmail.com>
2022-04-14 23:00:57 +0000

    task0-3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,19 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+/*
+ * File name: variadic_functions.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0F-variadic_functions directory.
+ */
+
 #include <stdarg.h>
 
 /**
- * struct printer - A new struct type defining a printer
- * @symbol: A symbol representing a data type
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
  * @print: A function pointer to a function that prints
- * a data type corresponding to symbol.
+ *         a data type corresponding to symbol.
  */
 typedef struct printer
 {
@@ -16,7 +22,7 @@ typedef struct printer
 
 } printer_t;
 
-int sum_them_all(const unsigned int n, ....);
+int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);

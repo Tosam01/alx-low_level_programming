076ad8b1bc594707b62b36337a17279e12d3150e
Tosam01 <tosam0001@gmail.com>
2022-04-25 23:15:06 +0000

    tk1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the no of elements in a linked list
- * @h: head of a list
+ * listint_len - returns the number of elements in
+ * a linked list.
+ * @h: head of a list.
  *
- * Return: no of nodes
+ * Return: numbers of nodes.
  */
-size_t listint_len(const listint *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t nnodes = 0;
 

c10af1dcb0ad4f76a1ae8bdef8c6b8818c2e2eab
Tosam01 <tosam0001@gmail.com>
2022-04-25 23:25:51 +0000

    tk2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,7 +8,7 @@
  *
  * Return: address of the new element. NULL if it failed
  */
-listint_t *add_node(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
